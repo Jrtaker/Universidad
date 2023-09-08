ba81eca476b7a5798b63f41d469e9eefa0ad85a6
Jrtaker <jrtaker45@gmail.com>
2023-09-08 12:49:31 -0300

    Conexion

--- src/universidadgrupo78/acesodatos/Conexion.java
+++ src/universidadgrupo78/acesodatos/Conexion.java
@@ -5,6 +5,13 @@
  */
 package universidadgrupo78.acesodatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Joni
@@ -12,5 +19,25 @@ package universidadgrupo78.acesodatos;
 public class Conexion {
     private static final String URL="jdbc:mariadb://localhost/";
     private static final String DB="universidad";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion (){}
+    
+    public static Connection getConexion(){
+        
+        if(connection ==null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver Java");
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);                
+            } catch (ClassNotFoundException ex){ 
+                JOptionPane.showMessageDialog(null, "Error al cargar los driver."); 
+            }catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Abrir XAMPP o algo esta mal escrito.");
+            }
+        }
+        return connection;
+    }
+}    
 
-}
