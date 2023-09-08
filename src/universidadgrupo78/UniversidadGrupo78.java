a2a009590385159925ee5688553574fb6b2af69f
Jrtaker <jrtaker45@gmail.com>
2023-09-08 13:01:11 -0300

    re:Driver

--- src/universidadgrupo78/UniversidadGrupo78.java
+++ src/universidadgrupo78/UniversidadGrupo78.java
@@ -1,12 +1,15 @@
 
 package universidadgrupo78;
 
+import java.sql.Connection;
+import universidadgrupo78.acesodatos.Conexion;
+
 
 public class UniversidadGrupo78 {
 
    
     public static void main(String[] args) {
-        
+        Connection con = Conexion.getConexion();
     }
     
 }
--- src/universidadgrupo78/acesodatos/Conexion.java
+++ src/universidadgrupo78/acesodatos/Conexion.java
@@ -8,8 +8,6 @@ package universidadgrupo78.acesodatos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -29,12 +27,13 @@ public class Conexion {
         
         if(connection ==null){
             try {
-                Class.forName("org.mariadb.jdbc.Driver Java");
+                Class.forName("org.mariadb.jdbc.Driver");
                 connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);                
+                JOptionPane.showMessageDialog(null, "Conectado"); 
             } catch (ClassNotFoundException ex){ 
                 JOptionPane.showMessageDialog(null, "Error al cargar los driver."); 
             }catch (SQLException ex) {
-                    JOptionPane.showMessageDialog(null, "Abrir XAMPP o algo esta mal escrito.");
+                JOptionPane.showMessageDialog(null, "Abrir XAMPP o algo esta mal escrito.");
             }
         }
         return connection;
