98d018db08a6289adc4768952985766df76a9a3a
Jrtaker <jrtaker45@gmail.com>
2023-09-19 12:40:32 -0300

    CambiodeDB

--- src/universidadgrupo78/acesodatos/Conexion.java
+++ src/universidadgrupo78/acesodatos/Conexion.java
@@ -13,7 +13,7 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
     private static final String URL="jdbc:mariadb://localhost/";
-    private static final String DB="universidad";
+    private static final String DB="universidadULP";
     private static final String USUARIO="root";
     private static final String PASSWORD="";
     private static Connection connection;
